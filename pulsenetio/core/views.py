6ebfcb7de4e33c96a6a820583da1eb22c708050b
Salay Abdul Muhaimin Kanton <helitoon@outlook.com>
2023-12-12 16:37:27 +0000

    Signup features

--- pulsenetio/core/views.py
+++ pulsenetio/core/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
 from django.http import HttpResponse
+from .models import Profile
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email is already taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username is taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                # LOg user in and dricte to settings page
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
+        else:
+            messages.info(request, 'Passwords Do not Match')
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html')
