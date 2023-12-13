b0820131b48efc1d8e822f72cc183c9195edfbe0
Salay Abdul Muhaimin Kanton <helitoon@outlook.com>
2023-12-13 19:12:22 +0000

    Post Liking Mechanism added

--- pulsenetio/core/admin.py
+++ pulsenetio/core/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Profile
-from .models import Post
-
+from .models import Profile, Post, LikePost
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Post)
+admin.site.register(LikePost)
