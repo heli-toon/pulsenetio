5385643b9edceddee8febb5c87458f141fc38995
Salay Abdul Muhaimin Kanton <helitoon@outlook.com>
2023-12-13 11:54:44 +0000

    Setting up account settings

--- pulsenetio/core/views.py
+++ pulsenetio/core/views.py
@@ -9,6 +9,31 @@ from .models import Profile
 def index(request):
     return render(request, 'index.html')
 
+@login_required(login_url='login')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+    if request.method == 'POST':
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        elif request.FILES.get('image') != None:
+            image =request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        return redirect('settings')
+    return render(request, 'settings.html', {'user_profile':user_profile})
+
 def signup(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -26,11 +51,13 @@ def signup(request):
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
 
-                # LOg user in and dricte to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('login')
+                return redirect('settings')
         else:
             messages.info(request, 'Passwords Do not Match')
             return redirect('signup')
@@ -51,8 +78,9 @@ def login(request):
             return redirect('login')
     else:
         return render(request, 'login.html')
-    
+
 @login_required(login_url='login')
 def logout(request):
     auth.logout(request)
     return redirect('login')
+
