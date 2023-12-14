b4e1754cb6ebc3fa1844d5791ae6d69b671108e4
Salay Abdul Muhaimin Kanton <helitoon@outlook.com>
2023-12-14 14:03:48 +0000

    Post Feed feature2 - Following posts

--- pulsenetio/core/views.py
+++ pulsenetio/core/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User, auth
 from django.contrib.auth.decorators import login_required
 from .models import Profile, Post, LikePost, FollowersCount
+from itertools import chain
 
 # Create your views here.
 @login_required(login_url='login')
@@ -10,8 +11,23 @@ def index(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
 
+    user_following_list = []
+    feed = []
+
+    user_following = FollowersCount.objects.filter(follower=request.user.username)
+     
+    for users in user_following:
+        user_following_list.append(users.user)
+    
+    for usernames in user_following_list:
+        feed_lists = Post.objects.filter(user=usernames)
+        feed.append(feed_lists)
+
+    feed_list = list(chain(*feed))
+
+
     posts = Post.objects.all()
-    return render(request, 'index.html', {'user_profile':user_profile, 'posts':posts})
+    return render(request, 'index.html', {'user_profile' : user_profile, 'posts': feed_list})
 
 @login_required(login_url='login')
 def settings(request):
