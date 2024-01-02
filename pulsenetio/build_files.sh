a229c2ef7e88b6b4786613ee0495e76081e724c0
Salay Abdul Muhaimin Kanton <helitoon@outlook.com>
2024-01-02 11:35:52 +0000

    deleted builds config

--- pulsenetio/build_files.sh
+++ pulsenetio/build_files.sh
@@ -8,4 +8,4 @@ python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 
 echo "Collecting static files..."
-python3 manage.py collectstatic --noinput
+python3 manage.py collectstatic
